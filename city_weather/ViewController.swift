fd3c001dac4b760eb818ab46f580aa2d4e060f3a
Alejandro <39561957+alejandro0709@users.noreply.github.com>
2022-01-31 22:30:33 -0500

    creating project

---
+++ city_weather/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  city_weather
+//
+//  Created by Alejandro Guerra, 1/31/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
